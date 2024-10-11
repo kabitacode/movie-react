2f4ca9bb0375ba18e19af8e7b6a0f6a2029d2b5b
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-11 15:48:28 +0700

    ✨

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 export const REACT_APP_API_KEY=process.env.REACT_APP_API_KEY;
-export const image_300 = process.env.image_300;
-export const unavailable = process.env.unavailable;
-
+export const image_300 = process.env.REACT_APP_IMAGE_300;
+export const unavailable = process.env.REACT_APP_UNAVAILABLE;
