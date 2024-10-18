c573a68bba093d6618f7f412a27d0d27c5554814
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-10-18 10:05:31 +0700

    add custom data

---
+++ src/data/index.ts
@@ -0,0 +1,22 @@
+export const dataButtonProfile = [
+    {
+        name: "Favorite Movie",
+        link: "favorite-movie",
+        icon: "heart"
+    },
+    {
+        name: "Favorite TV",
+        link: "favorite-tv",
+        icon: "tv"
+    },
+    {
+        name: "Watchlist Movie",
+        link: "watchlist-movie",
+        icon: "film"
+    },
+    {
+        name: "Watchlist TV",
+        link: "watchlist-tv",
+        icon: "tv"
+    },
+]
